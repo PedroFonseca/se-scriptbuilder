de6ad3f2bcdd4643415293a615e461076b914ced
Michael Smith <me@murray-mint.co.uk>
2015-04-13 21:28:50 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScriptBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program.DisplayBanner();
+
+            foreach (string arg in args)
+            {
+                switch (arg)
+                {
+                    case "--help":
+                    case "-h":
+                    case "/?":
+                        Program.Help();
+                        return;
+
+                    default:
+                        break;
+                }
+            }
+
+            Program.ProcessScript(args);
+        }
+
+        private static void ProcessScript(string[] args)
+        {
+            string sourceDir = Program.GetParam(args, 0);
+            string scriptName = Program.GetParam(args, 1);
+            string outputDir = Program.GetOutputDir();
+
+            Console.WriteLine(" Source: " + sourceDir);
+            Console.WriteLine(" Script Name: " + scriptName);
+            Console.WriteLine(" Output Dir: " + outputDir);
+
+            Script script = new Script(sourceDir, outputDir, scriptName);
+            script.Compile();
+            script.Write();
+        }
+
+        private static void Help()
+        {
+            Console.WriteLine("Usage: ScriptBuilder.exe <input> [scriptname]");
+        }
+
+        private static void DisplayBanner()
+        {
+            Console.WriteLine(Program.GetVersion() + Environment.NewLine);
+            Console.WriteLine("Michael Smith <me@murray-mint.co.uk>");
+            Console.WriteLine("https://github.com/murray-mint/se-scriptbuilder");
+            Console.WriteLine("This software is released under the MIT License" + Environment.NewLine);
+        }
+
+        private static string GetVersion()
+        {
+            Version version = Assembly.GetEntryAssembly().GetName().Version;
+            AssemblyTitleAttribute title = (AssemblyTitleAttribute)Assembly.GetEntryAssembly().GetCustomAttribute(typeof(AssemblyTitleAttribute));
+            return String.Format("{0} {1}.{2}.{3}", title.Title, version.Major.ToString(), version.Minor.ToString(), version.Revision.ToString());
+        }
+
+        private static string GetParam(string[] args, int index)
+        {
+            string defaultValue = null;
+            if (args.Length > index)
+            {
+                return args[index];
+            }
+            return defaultValue;
+        }
+
+        private static string GetOutputDir()
+        {
+            if (Environment.GetEnvironmentVariable("SE_SCRIPT_HOME") != null) {
+                return Environment.GetEnvironmentVariable("SE_SCRIPT_HOME");
+            }
+
+            string path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "SpaceEngineers", "IngameScripts", "local");
+            if (Directory.Exists(path)) {
+                return path;
+            } else {
+                throw new ArgumentException("Unable to find Space Engineers local script path");
+            }
+        }
+    }
+}
---
+++ Script.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScriptBuilder
+{
+    class Script
+    {
+        private string sourceDirectory;
+        private string outputDirectory;
+        private string scriptName;
+        private string output = "";
+
+        public Script (string sourceDirectory, string outputDirectory, string scriptName) {
+            this.sourceDirectory = Path.GetFullPath(sourceDirectory);
+            this.outputDirectory = Path.GetFullPath(outputDirectory);
+
+            this.scriptName = scriptName;
+            if (String.IsNullOrEmpty(this.scriptName))
+            {
+                this.scriptName = Path.GetFileName(this.sourceDirectory.TrimEnd(Path.DirectorySeparatorChar));
+            }
+        }
+
+        public void Compile()
+        {
+            // Special File - UserConfig.cs
+            string userConfig = Path.Combine(this.sourceDirectory, "UserConfig.cs");
+            if (File.Exists(userConfig))
+            {
+                this.output += this.ProcessFile(userConfig);
+            }
+            this.output += this.ProcessDirectory(this.sourceDirectory, 0);
+        }
+
+        public void Write()
+        {
+            string fullpath = Path.Combine(this.outputDirectory, this.scriptName);
+            if (!Directory.Exists(fullpath))
+            {
+                Directory.CreateDirectory(fullpath);
+            }
+            string filename = Path.Combine(fullpath, "Script.cs");
+            File.WriteAllText(filename, this.output);
+        }
+
+        private string ProcessDirectory(string directory, int level)
+        {
+            if (level > 128)
+            {
+                return "";
+            }
+
+            string output = "";
+            foreach (string dir in Directory.GetDirectories(directory))
+            {
+                output += this.ProcessDirectory(dir, level + 1);
+            }
+
+            string userConfig = Path.Combine(this.sourceDirectory, "UserConfig.cs");
+            foreach (string file in Directory.GetFiles(directory, "*.cs"))
+            {
+                if (file == userConfig)
+                {
+                    continue;
+                }
+                output += this.ProcessFile(file);
+            }
+
+            return output;
+        }
+
+        private string ProcessFile(string filename)
+        {
+            if (!File.Exists(filename))
+            {
+                return "";
+            }
+
+            string output = Environment.NewLine + "\t// File: " + Path.GetFileName(filename) + Environment.NewLine;
+            List<string> lines = File.ReadAllLines(filename).ToList<string>();
+
+            int startIndex = lines.FindIndex(x => x.Contains("#region SpaceEngineers"));
+            if (startIndex < 0)
+            {
+                return "";
+            }
+
+            int endIndex = lines.FindIndex(startIndex, x => x.Contains("#endregion"));
+            if (endIndex < 0)
+            {
+                return "";
+            }
+
+            int range = endIndex - startIndex - 1;
+            if (range < 0)
+            {
+                range = 0;
+            }
+
+            output += String.Join(Environment.NewLine, lines.GetRange(startIndex + 1, range));
+
+            output += Environment.NewLine;
+            return output;
+        }
+    }
+}
